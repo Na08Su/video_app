dc08f9283574c8e83a3a1d007cdde2bdf8f17536
Natsuki Sugawara <snsn19910803@gmail.com>
2016-03-01 13:16:46 +0900

    deploy

---
+++ app/models/video.rb
@@ -0,0 +1,11 @@
+class Video < ActiveRecord::Base
+
+  def next
+    #Video.find(id + 1)
+    Video.where(" id > ?", id).first
+  end
+  def prev
+    #Video.find(id - 1)
+    Video.where("id < ?", id).last
+  end
+end
