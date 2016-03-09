916fb91380e96b273016a41ace8a7f78c3ac4017
Natsuki Sugawara <snsn19910803@gmail.com>
2016-03-09 17:22:54 +0900

    video_app

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,10 +2,10 @@ class Video < ActiveRecord::Base
 
   def next
     #Video.find(id + 1)
-    Video.where(" id > ?", id).first
+    Video.where(" id > ?", id).first#次ページ
   end
   def prev
     #Video.find(id - 1)
-    Video.where("id < ?", id).last
+    Video.where("id < ?", id).last#前のページ
   end
 end
